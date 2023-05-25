d511b0c963158f027aeb0bc4f581c564d45d4eb1
vikas dakle <vikas@gmail.com>
2023-05-25 11:44:58 +0530

    initial commit

---
+++ app/projects/page.tsx
@@ -0,0 +1,140 @@
+
+const page = () => {
+    return <>
+
+        <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto flex flex-wrap">
+                <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+                    <img alt="feature" className="object-cover object-center h-full w-full" src="https://dummyimage.com/460x500" />
+                </div>
+                <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+                    <div className="flex flex-col mb-10 lg:items-start items-center">
+                        <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-6 h-6" viewBox="0 0 24 24">
+                                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                            </svg>
+                        </div>
+                        <div className="flex-grow">
+                            <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
+                            <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+                            <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="flex flex-col mb-10 lg:items-start items-center">
+                        <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-6 h-6" viewBox="0 0 24 24">
+                                <circle cx="6" cy="6" r="3"></circle>
+                                <circle cx="6" cy="18" r="3"></circle>
+                                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                            </svg>
+                        </div>
+                        <div className="flex-grow">
+                            <h2 className="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
+                            <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+                            <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="flex flex-col mb-10 lg:items-start items-center">
+                        <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-6 h-6" viewBox="0 0 24 24">
+                                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
+                            </svg>
+                        </div>
+                        <div className="flex-grow">
+                            <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
+                            <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+                            <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <Stat />
+        <Gallery />
+    </>
+}
+
+const Stat = () => {
+    return <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+            <div className="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
+                <div className="w-full sm:p-4 px-4 mb-6">
+                    <h1 className="title-font font-medium text-xl mb-2 text-gray-900">Moon hashtag pop-up try-hard offal truffaut</h1>
+                    <div className="leading-relaxed">Pour-over craft beer pug drinking vinegar live-edge gastropub, keytar neutra sustainable fingerstache kickstarter.</div>
+                </div>
+                <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+                    <h2 className="title-font font-medium text-3xl text-gray-900">2.7K</h2>
+                    <p className="leading-relaxed">Users</p>
+                </div>
+                <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+                    <h2 className="title-font font-medium text-3xl text-gray-900">1.8K</h2>
+                    <p className="leading-relaxed">Subscribes</p>
+                </div>
+                <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+                    <h2 className="title-font font-medium text-3xl text-gray-900">35</h2>
+                    <p className="leading-relaxed">Downloads</p>
+                </div>
+                <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
+                    <h2 className="title-font font-medium text-3xl text-gray-900">4</h2>
+                    <p className="leading-relaxed">Products</p>
+                </div>
+            </div>
+            <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
+                <img className="object-cover object-center w-full h-full" src="https://dummyimage.com/600x300" alt="stats" />
+            </div>
+        </div>
+    </section>
+
+}
+
+const Gallery = () => {
+    return <>
+        <section className="text-gray-600 body-font">
+            <div className="container px-5 py-24 mx-auto flex flex-wrap">
+                <div className="flex w-full mb-20 flex-wrap">
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">Master Cleanse Reliac Heirloom</h1>
+                    <p className="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
+                </div>
+                <div className="flex flex-wrap md:-m-2 -m-1">
+                    <div className="flex flex-wrap w-1/2">
+                        <div className="md:p-2 p-1 w-1/2">
+                            <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300" />
+                        </div>
+                        <div className="md:p-2 p-1 w-1/2">
+                            <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/501x301" />
+                        </div>
+                        <div className="md:p-2 p-1 w-full">
+                            <img alt="gallery" className="w-full h-full object-cover object-center block" src="https://dummyimage.com/600x360" />
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap w-1/2">
+                        <div className="md:p-2 p-1 w-full">
+                            <img alt="gallery" className="w-full h-full object-cover object-center block" src="https://dummyimage.com/601x361" />
+                        </div>
+                        <div className="md:p-2 p-1 w-1/2">
+                            <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/502x302" />
+                        </div>
+                        <div className="md:p-2 p-1 w-1/2">
+                            <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/503x303" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+}
+
+export default page
